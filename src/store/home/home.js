53dfd5eb27606b1340ca44db64142ea4652bb078
poper <poper@wangxutech.com>
2022-05-16 22:23:14 +0800

    首页3级分类完成

--- src/store/home/home.js
+++ src/store/home/home.js
@@ -1,9 +1,32 @@
-const state = {}
-const mutations = {}
-const actions = {}
-const getters = {}
+import {getCategoryList} from "@/api/api";
+
+const state = {
+  categoryList: []
+}
+const mutations = {
+  saveCategory(state, payload) {
+    state.categoryList = payload
+    state.categoryList.pop()
+  }
+}
+
+const actions = {
+  async getCategory(context) {
+    const data = await getCategoryList()
+    if (data['code'] === 200) {
+      context.commit('saveCategory', data.data)
+    }
+  }
+}
+
+const getters = {
+
+}
 
 
 export default {
-  state, mutations, getters, actions,
+  state,
+  mutations,
+  getters,
+  actions,
 }
